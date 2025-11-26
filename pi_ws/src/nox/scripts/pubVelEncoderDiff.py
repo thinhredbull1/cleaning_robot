00b4586b3a9dde056e92aa73487166c6d6a51b22
thinhhd6 <v.thinhhd6@vinmotion.net>
2025-11-26 17:10:17 +0700

    refactor esp control pi code and adding unit test odom and imu

--- pi_ws/src/nox/scripts/pubVelEncoderDiff.py
+++ pi_ws/src/nox/scripts/pubVelEncoderDiff.py
@@ -1,3 +1,4 @@
+from turtle import left, right
 import rospy
 from nav_msgs.msg import Odometry
 import tf
@@ -29,12 +30,12 @@ class MecanumRobot:
         self.onOffMor=False
         self.M_LEFT=0
         self.M_RIGHT=1
-        self.M_RIGHT_DOWN=0
+        self.M_RIGHT_DOWN=3
         self.acc_time=10
         self.dec_time=10
-        self.M_RIGHT_UP=1
-        self.M_LEFT_DOWN=2
-        self.M_LEFT_UP=3
+        self.M_RIGHT_UP=2
+        self.M_LEFT_DOWN=1
+        self.M_LEFT_UP=0
         self.coeff_speed=1.0
         self.moving=False
         # self.x_offset = 1.0  # Scaling factor for x
@@ -106,7 +107,7 @@ class MecanumRobot:
         self.speed_wheel_cm_s=[0,0,0,0]
         self.getCmdVel=False
         encoder_parameter= (math.pi * self.WHEEL_DIAMETER) / self.ENCODER_TOTAL
-        self.cmPerCount=[encoder_parameter,encoder_parameter,encoder_parameter,encoder_parameter] # 7.05
+        self.cmPerCount= encoder_parameter# 7.05
         # rospy.sleep(1.5)
         # self.motorUp=ZLAC8015D.Controller(modbus_connection,id=1) # 0 forn
         # self.motorDown=ZLAC8015D.Controller(modbus_connection,id=2) # 1
@@ -146,9 +147,9 @@ class MecanumRobot:
         delta=[0,0]
         delta_l_u,delta_r_u=self.CaldiffEncoder(self.M_LEFT_UP,self.M_RIGHT_UP)
         delta_l,delta_r=self.CaldiffEncoder(self.M_LEFT_DOWN,self.M_RIGHT_DOWN)
-        encoderTick=[delta_l_u,delta_r_u,delta_l,delta_r]
+        encoderTick=[delta_l_u,delta_l,delta_r_u,delta_r]
         for i in range(self.NMOTORS):
-            delta_tick[i] =  encoderTick[i] *self.cmPerCount[i]  # 0.14260
+            delta_tick[i] =  encoderTick[i] *self.cmPerCount  # 0.14260
         delta[self.M_LEFT]= (delta_tick[self.M_LEFT_UP]+delta_tick[self.M_LEFT_DOWN])/2.0
         delta[self.M_RIGHT]= (delta_tick[self.M_RIGHT_UP]+delta_tick[self.M_RIGHT_DOWN])/2.0
         dxy = (delta[self.M_LEFT]+delta[self.M_RIGHT])/2.0
@@ -268,12 +269,12 @@ class MecanumRobot:
         # print(speed_cm_s)
         for i in range(self.NMOTORS):
             # self.speed_desired[i] = speed_cm_s[i]*1.5 # to pulse / 10ms
-            self.speed_desired[i] = speed_cm_s[i] *((self.ms_pid/1000.0)/(self.cmPerCount[i]))
+            self.speed_desired[i] = speed_cm_s[i] *((self.ms_pid/1000.0)/(self.cmPerCount))
             self.speed_desired[i]=int(speed_cm_s[i])
         print(self.speed_desired)
     def runRobot(self):
         speed_wheel=[0,0,0,0]
-        max_speed=60
+        max_speed=30
         for i in range(self.NMOTORS):
             # if(i in self.inverseDir):
             #     self.speed_desired[i]=-self.speed_desired[i]
@@ -282,7 +283,7 @@ class MecanumRobot:
                 speed_wheel[i]=max_speed
             elif speed_wheel[i]<-max_speed:
                 speed_wheel[i]=-max_speed
-        serial_data = "{}/{}*{};".format(speed_wheel[self.M_LEFT_UP],speed_wheel[self.M_RIGHT_UP],speed_wheel[self.M_RIGHT_DOWN])
+        serial_data = "{}/{};".format(speed_wheel[self.M_LEFT_UP],speed_wheel[self.M_RIGHT_UP])
         self.moving=True
         # Gửi dữ liệu xuống serial
         # print(serial_data)
@@ -309,7 +310,7 @@ class MecanumRobot:
         print("Mode: "+str(self.test_mode))
         print("Wheel: "+str(self.WHEEL_DIAMETER))
         print("x_offset:"+str(self.x_offset))
-        print("cm_per_count:"+str(self.cmPerCount[0]))
+        print("cm_per_count:"+str(self.cmPerCount))
         while not rospy.is_shutdown():
             try:
                 if self.serial_port.in_waiting > 0:
@@ -317,12 +318,24 @@ class MecanumRobot:
                     data_line = self.serial_port.readline().decode('utf-8').strip()
                     # Giả sử định dạng dữ liệu từ STM32: "x/y&z*w\r\n"
                     # parts = data_line.split('/')
-                    parts = data_line.replace('*', '/').split('/')
-                    if len(parts) == 3:
+                    parts = data_line.replace(";", "")
+
+                    # Tách lần lượt trái và phải
+                    # Trái: L_up / L_down
+                    # Phải: R_up * R_down
+                    left_right = parts.split("&")
+                    if len(left_right) == 2:
+                        left_ = left_right[0].split("/")      # [L_up, L_down]
+                        right_ = left_right[1].split("*")     # [R_up, R_down]
                         self.encoder_total[0] = int(parts[0])
                         self.encoder_total[1] = int(parts[1])
+                        if len(left_) == 2 and len(right_) == 2:
+                            self.encoder_total[0] = int(left_[0])     # L_up
+                            self.encoder_total[1] = int(left_[1])     # L_down
+                            self.encoder_total[2] = int(right_[0])    # R_up
+                            self.encoder_total[3] = int(right_[1])    # R_down
                         # self.dtheta=float(parts[2])/100.0
-                        self.updatePos()
+                            self.updatePos()
                         # rospy.loginfo(f"Encoders: {self.encoder_total}")
             except Exception as e:
                 rospy.logwarn(f"Error reading serial data: {e}")
